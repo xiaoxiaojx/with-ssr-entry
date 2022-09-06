31c53876bfa09e38c2742ef4316c415d6cda74d9
xiao252 <2529897657@qq.com>
2022-09-06 13:48:54 +0800

    nextjs v12 存在config.module.rules[3].oneOf为undefined情况

--- src/with-ssr-entry.ts
+++ src/with-ssr-entry.ts
@@ -38,43 +38,44 @@ export default function (nextConfig: Record<string, any> = {}) {
           };
         }
       }
-
-      // @ts-ignore
-      config.module.rules[3].oneOf = config.module.rules[3].oneOf.filter(
+      if(config.module.rules[3].oneOf){
         // @ts-ignore
-        (item) => {
+        config.module.rules[3].oneOf = config.module.rules[3].oneOf.filter(
           // @ts-ignore
-          if (item.use && item.use.loader === "error-loader") {
-            return false;
-          }
+          (item) => {
+            // @ts-ignore
+            if (item.use && item.use.loader === "error-loader") {
+              return false;
+            }
 
-          if (
-            item.issuer &&
-            // @ts-ignore
-            Array.isArray(item.issuer.not) &&
-            // @ts-ignore
-            item.issuer.not.find((i) => i.toString() === "/node_modules/")
-          ) {
-            // @ts-ignore
-            item.issuer.not = item.issuer.not.filter(
+            if (
+              item.issuer &&
               // @ts-ignore
-              (i) => i.toString() !== "/node_modules/"
-            );
+              Array.isArray(item.issuer.not) &&
+              // @ts-ignore
+              item.issuer.not.find((i) => i.toString() === "/node_modules/")
+            ) {
+              // @ts-ignore
+              item.issuer.not = item.issuer.not.filter(
+                // @ts-ignore
+                (i) => i.toString() !== "/node_modules/"
+              );
+            }
+            if (
+              item.issuer &&
+              // @ts-ignore
+              typeof item.issuer.not === "string" &&
+              // @ts-ignore
+              item.issuer.not.toString() === "/node_modules/"
+            ) {
+              // @ts-ignore
+              item.issuer.not = [];
+            }
+            return true;
           }
-          if (
-            item.issuer &&
-            // @ts-ignore
-            typeof item.issuer.not === "string" &&
-            // @ts-ignore
-            item.issuer.not.toString() === "/node_modules/"
-          ) {
-            // @ts-ignore
-            item.issuer.not = [];
-          }
-          return true;
-        }
-      );
-
+        );
+      }
+      
       if (typeof nextConfig.webpack === "function") {
         return nextConfig.webpack(config, options);
       }
